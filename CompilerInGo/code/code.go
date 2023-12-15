2fb4004890e45649746825774a0b6ab065b534f7
xwj <xwj@stu.pku.edu.cn>
2023-12-15 16:10:07 +0800

    开端

--- CompilerInGo/code/code.go
+++ CompilerInGo/code/code.go
@@ -1,1 +1,13 @@
+// code/code.go
+
 package code
+
+// 字节的集合
+type Instructions []byte
+
+// 操作码
+type Opcode byte
+
+const (
+	OpConstant Opcode = iota
+)
---
+++ CompilerInGo/go.mod
@@ -0,0 +1,3 @@
+module compilerInGo
+
+go 1.19
